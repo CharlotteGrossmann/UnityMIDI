caddb956d6878ef7d9d54de21153be9fc770c2a9
CharlotteGrossmann <charlotte.grossmann@stud.h-da.de>
2021-01-08 13:08:52 +0100

    first upload

---
+++ MIDI-1.1/Assets/MidiMessageDistributor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MidiMessageDistributor : MonoBehaviour
+{
+    public GameObject[] targets;
+    MidiReceiver receiver;
+
+    void Start ()
+    {
+        receiver = FindObjectOfType (typeof(MidiReceiver)) as MidiReceiver;
+    }
+
+    void Update ()
+    {
+        while (!receiver.IsEmpty) {
+            var message = receiver.PopMessage ();
+            if (message.status == 0x90) { //message.status überprüft den ersten Byte der MIDI Nachricht. 0x90 heíßt Standartmäßig Note ON
+                foreach (var go in targets) {
+                    go.SendMessage ("OnNoteOn", message);
+                }
+            } else if (message.status == 0x80) { //0x80 ist im MIDI Standard der Code für Note OFF
+                foreach (var go in targets) {
+                    go.SendMessage ("OnNoteOff", message);
+                }
+            }
+        }
+    }
+}
