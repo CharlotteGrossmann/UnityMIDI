1df440b9778cc98f1d14d172f5e371ce63c69099
CharlotteGrossmann <charlotte.grossmann@stud.h-da.de>
2021-01-27 17:36:59 +0100

    deleted unnecessary stuff

---
+++ MIDI-1.1/Assets/PlugIns/BanyanUnityPackage/Scripts/MessageProcessor.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+//summary
+// This script does an action based on the message forwarded by BanyanMessageListener (sent by Banyan).
+// This script should be included on any object that will be doing an action, based on a message coming from Banyan
+//summary
+
+public class MessageProcessor : MonoBehaviour
+{
+    private int sensorValue;
+    private string component;
+    private string secondaryValue;
+    private string lastValue;
+    public int pressure;
+    public int note;
+    public int pitch = 64;
+    public int sustain;
+    public bool JoyStickButton;
+
+    public void DoAction(string action, string info, int value, string target)
+    {
+        sensorValue = value;
+        component = action;
+        secondaryValue = info;
+        lastValue = target;
+
+    }
+    void Update()
+    {
+        if (component == "rhythm")
+            pressure = sensorValue;
+        else if (component == "melody")
+            note = sensorValue;
+            
+        else if (component == "modulate")
+        {
+            pitch = sensorValue;
+            sustain = int.Parse(secondaryValue);
+            // JoyStickButton = bool.Parse(lastValue);
+        }
+        else
+            pitch = 500; //Standart-Wert für Pitch
+    }   
+}
---
+++ MIDI-1.1/Assets/Scripts/group_visualizer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class group_visualizer : MonoBehaviour
+{
+    public GameObject Instrument; //Import MessageProcessor.cs
+    public MeshRenderer MelodyIndicator;
+    public MeshRenderer RhythmIndicator;
+    public MeshRenderer ModulateIndicator;
+
+    public int PitchChange; //Modulation der Tonhöhe -> Joystick
+    public int SustainChange; //Modulation des Klangs -> Joystick
+    public int NoteChange; //Melodie -> Buttons
+    private int PressureChange; //Rythmus -> Force Sensitive Resistor
+   
+    void Update()
+    {
+        PitchChange = Instrument.GetComponent<MessageProcessor>().pitch;
+        PressureChange = Instrument.GetComponent<MessageProcessor>().pressure;
+        NoteChange = Instrument.GetComponent<MessageProcessor>().note;
+        SustainChange = Instrument.GetComponent<MessageProcessor>().sustain;
+
+        if (NoteChange != -1) //im Raspi Code steht am Anfang noch Note = 0, das muss zu Note = -1 geändert werden
+            MelodyIndicator.enabled = true;
+        else
+            MelodyIndicator.enabled = false;
+
+        if (PressureChange != 0)             // der sensor ist ziemlich entfindlich, es ist nicht schlimm das die Anzeig so flackert
+            RhythmIndicator.enabled = true;
+        else
+            RhythmIndicator.enabled = false;
+
+        if (/*SustainChange != 500||*/PitchChange != 500) //pitch wird manchmal nicht richtig auf 500 gesetzt sondern hängt bei 800 oder 300
+            ModulateIndicator.enabled = true;
+        else
+            ModulateIndicator.enabled = false;
+
+
+    }
+}
