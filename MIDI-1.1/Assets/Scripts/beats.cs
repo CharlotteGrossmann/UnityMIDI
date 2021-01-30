794354d7130b7979e5b16021014cf9665400a770
CharlotteGrossmann <charlotte.grossmann@stud.h-da.de>
2021-01-30 20:16:35 +0100

    personal display

---
+++ MIDI-1.1/Assets/Scripts/beats.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class beats : MonoBehaviour
+{
+    private Transform[] allBeats;
+    public int i = 1;
+    private bool beatOn = false;
+    private bool first = true;
+    void Start()
+    {
+        allBeats = GetComponentsInChildren<Transform>();
+    }
+
+    void Update()
+    {
+        
+        if (!beatOn)
+        {
+            InvokeRepeating("BeatBeats", 2f, 2f);
+            beatOn = true;
+        }
+      
+       
+    }
+
+    void BeatBeats()
+    {
+        if (beatOn)
+        {
+
+            //mulitply desired movement by Time.deltaTime to move per second
+            if (i != 1)
+                allBeats[i - 1].transform.position = new Vector3(allBeats[i - 1].transform.position.x, allBeats[i-1].transform.position.y - 30f, allBeats[i - 1].transform.position.z);
+            if(i==1 && !first)
+                allBeats[12].transform.position = new Vector3(allBeats[12].transform.position.x, allBeats[12].transform.position.y - 30f, allBeats[12].transform.position.z);
+
+            allBeats[i].transform.position = new Vector3(allBeats[i].transform.position.x, allBeats[i].transform.position.y+30f , allBeats[i].transform.position.z);
+            
+            if(i<allBeats.Length)
+                i+=1;
+            if (i == allBeats.Length)
+                i = 1;
+            first = false;
+            beatOn = false;
+            
+        }
+        
+
+    }
+}
+
+
